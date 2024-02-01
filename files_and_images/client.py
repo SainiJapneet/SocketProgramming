8850de0ba0166df2a45bd8044ad312d36ec10efd
japneet <japneetsaini084@gmail.com>
2024-02-01 22:04:03 +0530

    image transfer

--- files_and_images/client.py
+++ files_and_images/client.py
@@ -3,13 +3,29 @@ import socket
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as conn:
     conn.connect((socket.gethostname(),4571))
 
+    '''
     custom_file = open('client_files/received_file.txt','wb')
+    i = 1
     while True:
         data = conn.recv(40960)
         if not data:
-            print('No message from the serve. Closing the connection...')
+            print('No message from the server. Closing the connection...')
             break
         custom_file.write(data)
-        print('Batch of data written to file...')
+        print('Batch ', i ,' of data written to file...')
+        i += 1
 
-    custom_file.close
+    custom_file.close()
+    '''
+
+    with open('client_files/received_image.jpg','wb') as image_file:
+        i = 1
+        while True:
+            data = conn.recv(40960)
+            if not data:
+                print('No message from the server. VClosing the connection...')
+                break
+
+            image_file.write(data)
+            print('Batch ', i ,' of data written to file...')
+            i += 1
--- files_and_images/server.py
+++ files_and_images/server.py
@@ -1,6 +1,7 @@
 import socket
-
-#socket.AF_INET specifies that we are going to attcah the socket to address specified by hostname and socket number
+from PIL import Image
+#PIL is Python imaging Library used to open/manipulate and perform other operations on an image
+#socket.AF_INET specifies that we are going to attach the socket to address specified by hostname and socket number
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as conn:
     conn.bind((socket.gethostname(),4571))
     conn.listen(5)
@@ -9,10 +10,23 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as conn:
     client, address = conn.accept()
     print('Connection to : ',address,' established\n')
     print('Client object : ',client,'\n')
-    custom_file = open('server_files/archive/pg10.txt','rb')#rb is used to specify that we are opening file in read only mode and in binary format
+
+    '''
+    custom_file = open('server_files/pg10.txt','rb')#rb is used to specify that we are opening file in read only mode and in binary format
     
     custom_data = custom_file.read(40960)# we will read 40Kb of data in a go.
     while(custom_data):
         client.send(custom_data)
         custom_data = custom_file.read(40960)
     print('Custom file sent successfully')
+    '''
+
+    with open('server_files/demo_img.jpg','rb') as image_file:
+        image_batch = image_file.read(40960)
+        i = 1
+        while(image_batch):
+            client.send(image_batch)
+            image_batch = image_file.read(40960)
+            print('Batch ', i ,'sent to client...')
+            i += 1
+print('Image sent successfully')
